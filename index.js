7f74b950220938f966de1b0cd610fbf55d541e2e
ArthiSiva <preethissap@gmail.com>
2022-11-09 23:38:26 +0530

    Added function to display time

---
+++ index.js
@@ -0,0 +1,25 @@
+const hourEl=document.getElementById("hour")
+const minEl=document.getElementById("minutes")
+const secEl=document.getElementById("seconds")
+const amEl=document.getElementById("ampm");
+function updateclock(){
+    let h= new Date().getHours()
+    let m= new Date().getMinutes()
+    let s=new Date().getSeconds()
+    let ampm="AM"
+
+    if(h>12){
+        h=h-12;
+        ampm="PM";
+    }
+    h= h<10 ? "0"+ h:h;
+    m= m<10 ? "0"+m : m; 
+    s= s<10 ? "0"+s : s;
+
+    hourEl.innerText=h;
+    minEl.innerText=m;
+    secEl.innerText=s;
+    amEl.innerText=ampm;
+    setTimeout(()=>{updateclock()},1000)
+}
+updateclock()
